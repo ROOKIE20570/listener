bb07233a89f6a599f5f9bfe4327352fa84e5274e
lixiaoyi <422567988@qq.com>
2019-10-01 22:07:51 +0800

    add redis

--- parser/parser.go
+++ parser/parser.go
@@ -1,10 +1,5 @@
 package parser
 
-type plugin struct {
-	filter string
-	port int
-}
-
 type parser interface {
 	 //对应每个parser不同的过滤行为
 	resolve (stream []byte) (string, error)//解析函数
---
+++ parser/redis.go
@@ -0,0 +1,12 @@
+package parser
+
+type Redis struct {
+}
+
+func (redis *Redis) resolve(stream []byte) (string, error) {
+
+}
+
+func (redis *Redis) getFilter(port int) string {
+
+}
