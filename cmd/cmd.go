2aaec0f6f70fbf6a73b19ecd172a92587397d949
lixiaoyi <422567988@qq.com>
2019-10-02 23:25:15 +0800

    fix

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,40 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
-	"log"
-	"time"
-)
-
 var (
-	device       string = "lo0"
-	snapshot_len int32  = 1024
-	promiscuous  bool   = false
-	err          error
-	timeout      time.Duration = 30 * time.Second
-	handle       pcap.Handle
+	port int
+	name string
 )
 
 func main() {
-	//Open device
-	handle, err := pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer handle.Close()
 
-	// Use the handle as a packet source to process all packets
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		// Process packet here
-		tcpLayer := packet.Layer(layers.LayerTypeTCP)
-		if tcpLayer != nil{
-			_, some := tcpLayer.(*layers.TCP)
-			fmt.Println(some)
-		}
-	}
+	////Open device
+	//handle, err := pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer handle.Close()
+	//
+	//// Use the handle as a packet source to process all packets
+	//packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	//for packet := range packetSource.Packets() {
+	//	// Process packet here
+	//	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	//	if tcpLayer != nil{
+	//		_, some := tcpLayer.(*layers.TCP)
+	//		fmt.Println(some)
+	//	}
+	//}
+
 
 }
