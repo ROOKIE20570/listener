b1e24eb5db54edf261ba98d835f2f380c69ab272
lixiaoyi <422567988@qq.com>
2019-10-03 23:03:42 +0800

    addFilter

--- core/manager.go
+++ core/manager.go
@@ -5,12 +5,15 @@ import "log"
 func run(device string, port int){
 
 	handle, err := pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	var parse *parser.Parser
 	switch device {
 	case "mysql":
-		parse := new(parser.Mysql)
+		parse = new(parser.Mysql)
 	case "redis":
-		parse := new(parser.Redis)
+		parse = new(parser.Redis)
 	default:
 		log.Fatal("暂不支持的设备")
 	}
+
+	handle.setBPFFilter(parse.getFilter())
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,6 @@
 package parser
 
-type parser interface {
+type Parser interface {
 	 //对应每个parser不同的过滤行为
 	resolve (stream []byte) (string, error)//解析函数
 	getFilter(port int) string
