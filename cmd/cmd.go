bfcd3e81297596f260a82750f4f0ee5ab2d4e3bc
lixiaoyi <422567988@qq.com>
2019-10-08 20:35:04 +0800

    remove more

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -16,23 +16,6 @@ func main() {
 	flag.StringVar(&device,"d","","device name")
 
 	core.Run(device,port)
-	////Open device
-	//handle, err := pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer handle.Close()
-	//
-	//// Use the handle as a packet source to process all packets
-	//packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	//for packet := range packetSource.Packets() {
-	//	// Process packet here
-	//	tcpLayer := packet.Layer(layers.LayerTypeTCP)
-	//	if tcpLayer != nil{
-	//		_, some := tcpLayer.(*layers.TCP)
-	//		fmt.Println(some)
-	//	}
-	//}
 
 
 }
